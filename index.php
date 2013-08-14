6c418372e9a9eddb6eb53400d3fef0c770ea449c
ripasaha <ripalogging@gmail.com>
2013-08-14 15:25:58 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+</head>
+<body>
+	This is the default page. Clicking the link below will login the user.
+	<a href="first.php">Click here to login</a>
+	<br/>
+	<br/>
+	<br/>
+	<p>
+	<a href="">Back to article</a>
+	</p>
+</body>
+</html>
