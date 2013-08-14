72a077343dd5aa6f7ce168f270d3c4b72bb857d5
ripasaha <ripalogging@gmail.com>
2013-08-14 15:24:19 +0530

    Create check.php

---
+++ check.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+$stillLoggedIn = timeCheck();
+echo $stillLoggedIn;
+
+function timeCheck()
+{
+	if(!isset($_SESSION['isLoggedIn']) || !($_SESSION['isLoggedIn']))
+	{
+		session_unset();
+		return true;
+		exit;
+	}
+	else
+	{
+		// user is logged in
+		require 'timeCheck.php';
+		$hasSessionExpired = checkIfTimedOut();
+		if($hasSessionExpired)
+		{
+			session_unset();
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
+?>
